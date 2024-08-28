c5135b759034015441836f69e369e7ee7f1044c9
Amirol Zolkifli <sitehandy@gmail.com>
2024-08-28 09:43:04 +0800

    Update route

--- routes/web.php
+++ routes/web.php
@@ -16,23 +16,23 @@ Route::get('/login', [LoginController::class, 'paparkanBorangLogin']);
 Route::post('/login', [LoginController::class, 'dapatkanDataLogin']);
 
 // Halaman Dashboard ('/dashboard')
-Route::get('/dashboard', DashboardController::class);
+Route::get('/ruangan-ahli-yang-baru', DashboardController::class)->name('gelaran.dashboard');
 // Route::get(uri, callback);
 
 // Halaman Senarai Users ('/users')
-Route::get('/users', [UserController::class, 'getSenaraiUsers']);
+Route::get('/users', [UserController::class, 'getSenaraiUsers'])->name('users.getSenaraiUsers'); // users.index
 // Routing untuk memaparkan borang daftar user
-Route::get('/users/new', [UserController::class, 'paparBorangUserBaru']);
+Route::get('/users/new', [UserController::class, 'paparBorangUserBaru'])->name('users.paparBorangUserBaru'); // users.create
 // Routing untuk menyimpan rekod daftar user baru
-Route::post('/users/new', [UserController::class, 'simpanRekodUserBaru']);
+Route::post('/users/new', [UserController::class, 'simpanRekodUserBaru'])->name('users.simpanRekodUserBaru'); // users.store
 // Routing untuk memaparkan detail user
-Route::get('/users/{id}', [UserController::class, 'paparDetailUser']);
+Route::get('/users/{id}', [UserController::class, 'paparDetailUser'])->name('users.paparDetailUser'); // users.show
 // Routing untuk memaparkan borang kemaskini rekod user
-Route::get('/users/{id}/edit', [UserController::class, 'paparBorangEditUser']);
+Route::get('/users/{id}/edit', [UserController::class, 'paparBorangEditUser'])->name('users.paparBorangEditUser'); // users.edit
 // Routing untuk menyimpan rekod kemaskini user
-Route::patch('/users/{id}/edit', [UserController::class, 'simpanRekodEditUser']);
+Route::patch('/users/{id}/edit', [UserController::class, 'simpanRekodEditUser'])->name('users.simpanRekodEditUser'); // users.update
 // Routing untuk menghapuskan rekod user
-Route::delete('/users/{id}', [UserController::class, 'deleteRekodUser']);
+Route::delete('/users/{id}', [UserController::class, 'deleteRekodUser'])->name('users.deleteRekodUser'); // users.destroy
 
 
 
