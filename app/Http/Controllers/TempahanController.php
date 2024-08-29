490735adb99629a8634e7a0772d109f1b2335715
Amirol Zolkifli <sitehandy@gmail.com>
2024-08-29 15:01:32 +0800

    Update model

--- app/Http/Controllers/TempahanController.php
+++ app/Http/Controllers/TempahanController.php
@@ -28,7 +28,41 @@ class TempahanController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        // Validate data dan ambil data tersebut
+        $data = $request->validate( [
+            'jenis_kenderaan' => ['required'],
+            'no_kenderaan' => ['required'],
+            'tarikh_tempahan' => ['required', 'date'],
+            'tarikh_mula' => ['required', 'date'],
+            'tarikh_akhir' => ['required', 'date'],
+            'nama_pemandu' => ['required'],
+            'tujuan' => ['required'],
+            'alamat_destinasi' => ['required'],
+            'status' => ['required']
+        ] );
+
+        // Simpan data ke dalam table tempahan
+        // Cara 1 Simpan data menggunakan model
+        // $tempahan = new Tempahan;
+
+        // $tempahan->jenis_kenderaan = $data['jenis_kenderaan'];
+        // $tempahan->no_kenderaan = $data['no_kenderaan'];
+        // $tempahan->tarikh_tempahan = $data['tarikh_tempahan'];
+        // $tempahan->tarikh_mula = $data['tarikh_mula'];
+        // $tempahan->tarikh_akhir = $data['tarikh_akhir'];
+        // $tempahan->nama_pemandu = $data['nama_pemandu'];
+        // $tempahan->tujuan = $data['tujuan'];
+        // $tempahan->alamat_destinasi = $data['alamat_destinasi'];
+        // $tempahan->status = $data['status'];
+
+        // $tempahan->save();
+
+        Tempahan::create($data);
+
+        // Bagi respon berjaya
+        return redirect()->route('tempahan.index')->with('mesej-berjaya', 'Rekod berjaya ditambah');
+
+
     }
 
     /**
--- app/Models/Tempahan.php
+++ app/Models/Tempahan.php
@@ -8,4 +8,20 @@ use Illuminate\Database\Eloquent\Model;
 class Tempahan extends Model
 {
     use HasFactory;
+
+    // Maklumkan kepada model Tempahan nama table yang perlu dihubungi
+    protected $table = 'tempahan';
+
+    // Maklumkan kepada model Tempahan data yang dibenarkan masuk ke table tempahan
+    protected $fillable = [
+        'jenis_kenderaan',
+        'no_kenderaan',
+        'nama_pemandu',
+        'tarikh_tempahan',
+        'tarikh_mula',
+        'tarikh_akhir',
+        'tujuan',
+        'alamat_destinasi',
+        'status'
+    ];
 }
